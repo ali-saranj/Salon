af5eb5ad0b1e959f37129b6ff63819e738a14762
delta <amin138400138400@gmail.com>
2023-11-02 11:56:21 +0330

    Amin

---
+++ app/src/main/java/com/example/salon/ui/activity/Show_single_Salon.java
@@ -0,0 +1,16 @@
+package com.example.salon.ui.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.salon.R;
+
+public class Show_single_Salon extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_single_salon);
+    }
+}
